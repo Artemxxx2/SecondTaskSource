db9a9f64ece71d6bc988c16b01b550d9917204af
Artemxxx2 <akolobnevstudio@gmail.com>
2023-07-09 17:19:04 +0300

    GetData was changed

--- getData.py
+++ getData.py
@@ -1,35 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import keyboard
 
-import time
-
 def BtnClickAhead():
+    wait = WebDriverWait(driver, 15)  
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'submit-button')))
     submit = driver.find_element(By.CLASS_NAME, 'submit-button')
     submit.click()
-    time.sleep(delay)
+    wait = WebDriverWait(driver, 15)  
+    wait.until(EC.presence_of_element_located((By.XPATH, '//button[@title="Continue with verification process"]')))
     btn = driver.find_element(By.XPATH, '//button[@title="Continue with verification process"]')
     btn.click()
-    time.sleep(delay)
 def BtnClickBack():
+    wait = WebDriverWait(driver, 15)  
+    wait.until(EC.presence_of_element_located((By.XPATH, '//button[@title="common.backHint"]')))
     btnBack1 = driver.find_element(By.XPATH, '//button[@title="common.backHint"]')
     btnBack1.click()
-    time.sleep(delay)
+    wait = WebDriverWait(driver, 15)  
+    wait.until(EC.presence_of_element_located((By.XPATH, '//span[text()="Back"]')))
     btnBack2 = driver.find_element(By.XPATH, '//span[text()="Back"]') 
     btnBack2.click()
-    time.sleep(delay)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_extension('./zip.zip')
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 15.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36")
 
 emailWithoutDomain = input('Please, enter an email without domain(Mohdabo2023+testDDD=)\n')
 domain = input('Please, enter you domain(@gmail.com) \n')
 startNumber = input('Please, enter number,from which you want to start \n')
 endNumber = input('Please, enter number where you want to end \n')
-delay = input('Please, enter your delay \n')
-delay = int(delay)
 startNumber = int(startNumber)
 endNumber = int(endNumber)
 
@@ -60,7 +63,8 @@ BtnClickAhead()
 BtnClickBack()
 
 while startNumber <= endNumber:
-    time.sleep(delay)
+    wait = WebDriverWait(driver, 15)  
+    wait.until(EC.presence_of_element_located((By.XPATH, '//input[@formcontrolname="email"]')))
     email = driver.find_element(By.XPATH, '//input[@formcontrolname="email"]')
     email.send_keys(Keys.BACKSPACE * len(email.get_attribute('value')))
     email.send_keys(emailWithoutDomain + str(startNumber) + domain)
